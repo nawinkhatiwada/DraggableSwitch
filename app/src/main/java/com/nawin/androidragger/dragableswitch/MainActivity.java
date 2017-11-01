30be3613db8277f0ff34fe88c265e1b8d1a5156d
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-11-01 15:49:03 +0545

    listener added

--- app/src/main/java/com/nawin/androidragger/dragableswitch/MainActivity.java
+++ app/src/main/java/com/nawin/androidragger/dragableswitch/MainActivity.java
@@ -2,13 +2,34 @@ package com.nawin.androidragger.dragableswitch;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+import com.nawin.androidragger.switchlibrary.DraggableSwitch;
+
+public class MainActivity extends AppCompatActivity {
+    DraggableSwitch draggableSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        draggableSwitch = (DraggableSwitch) findViewById(R.id.draggableSwitch);
+        draggableSwitch.setChecked(false);
+        draggableSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked){
+                        Toast.makeText(MainActivity.this, "dekheo ta yar", Toast.LENGTH_LONG).show();
+                    }else {
+                        Toast.makeText(MainActivity.this, "dekhena ta yar", Toast.LENGTH_LONG).show();
+
+                    }
+            }
+        });
 
     }
+
 }
